c1fb8d5cc8e4f5f0b340d19e527f133a974367a6
Dave Lee <isho.lee@gmail.com>
2020-12-03 16:22:59 +0800

    added API key's model and service, service to implement

--- models/apikey.model.js
+++ models/apikey.model.js
@@ -1,46 +1,51 @@
-const { Sequelize } = require('sequelize');
+const { DataTypes, Sequelize, Model } = require('sequelize');
+const db = require('./db');
 
-module.exports = function (sequelize, DataTypes) {
-  return sequelize.define('ApiKey', {
-    id: {
-      type: DataTypes.UUID,
-      // defaultValue: sequelize.literal('gen_random_uuid()'),
-      defaultValue: Sequelize.UUIDV4, // gen_random_uuid() returns a version 4 UUID.
-      allowNull: false,
-      primaryKey: truex
-    },
-    key: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    description: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    status: {
-      type: DataTypes.ENUM(['Active', 'Suspended']),
-      defaultValue: 'Active',
-      allowNull: false
-    },
-    role: {
-      type: DataTypes.ENUM(['SUPERADMIN', 'ADMIN', 'VIEW']),
-      defaultValue: 'ADMIN',
-      allowNull: false
-    },
-    creationTime: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-      allowNull: false
-    },
-    lastModified: {
-      type: DataTypes.DATE
-    },
-    username: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    }
-  }, {
-    tableName: 'apikeys',
-    timestamps: false
-  });
-};
+class ApiKey extends Model { }
+
+ApiKey.init({
+  id: {
+    type: DataTypes.UUID,
+    // defaultValue: sequelize.literal('gen_random_uuid()'),
+    defaultValue: Sequelize.UUIDV4, // gen_random_uuid() returns a version 4 UUID.
+    allowNull: false,
+    primaryKey: truex
+  },
+  key: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  status: {
+    type: DataTypes.ENUM(['Active', 'Suspended']),
+    defaultValue: 'Active',
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.ENUM(['SUPERADMIN', 'ADMIN', 'VIEW']),
+    defaultValue: 'ADMIN',
+    allowNull: false
+  },
+  creationTime: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+    allowNull: false
+  },
+  lastModified: {
+    type: DataTypes.DATE
+  },
+  username: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  }
+}, {
+  sequelize: db,
+  modelName: 'ApiKey',
+  tableName: 'apikeys',
+  timestamps: false
+});
+
+module.exports = ApiKey;
---
+++ services/apikey.service.js
@@ -0,0 +1,26 @@
+const ApiKey = require('../models/apikey.model');
+
+/**
+ * Create an API key
+ */
+// TODO
+
+/**
+ * Get a list of API keys
+ */
+// TODO
+
+/**
+ * Bulk update status
+ */
+// TODO
+
+/**
+ * Delete an API key
+ */
+// TODO
+
+/**
+ * Bulk delete API keys
+ */
+// TODO
